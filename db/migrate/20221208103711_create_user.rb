4fbeb9b92c98daeee7f0bdf2b5c3c8dc66e82e88
Megha Bodke <megha.n.bodke@gmail.com>
2022-12-08 16:53:15 +0530

    Create models and add foreign keys

---
+++ db/migrate/20221208103711_create_user.rb
@@ -0,0 +1,11 @@
+class CreateUser < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :bio
+      t.string :photo
+      t.string :posts_counter
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20221208104744_create_post.rb
@@ -0,0 +1,11 @@
+class CreatePost < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :text
+      t.string :comments_counter
+      t.string :likes_counter
+      t.timestamps
+    end
+  end
+end
