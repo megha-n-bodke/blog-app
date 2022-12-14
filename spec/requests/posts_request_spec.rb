6baf45e1601aeb6aa7d5e97d70393fb3506d2969
Megha Bodke <megha.n.bodke@gmail.com>
2022-12-14 16:57:28 +0530

    Add test for posts controller

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe 'Posts', type: :request do
     it 'returns http success' do
       get '/posts/index'
       expect(response).to have_http_status(:success)
+      expect(response.body).to include('Here is a list of posts for a given user')
     end
   end
 
@@ -12,6 +13,7 @@ RSpec.describe 'Posts', type: :request do
     it 'returns http success' do
       get '/posts/show'
       expect(response).to have_http_status(:success)
+      expect(response.body).to include('Posts#show')
     end
   end
 end
