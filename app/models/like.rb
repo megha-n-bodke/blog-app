40f7e95a5b799f006f4be9558bd06b01f6619bcb
Megha Bodke <megha.n.bodke@gmail.com>
2022-12-18 00:43:45 +0530

    Add comment, add likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,7 @@ class Like < ActiveRecord::Base
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
+  after_save :update_like_count
   def update_like_count
     post.increment!(:likes_counter)
   end
