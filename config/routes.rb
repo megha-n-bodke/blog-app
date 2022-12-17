b795671e0f8dada737fe8a8bf5abfb9b043488b3
Megha Bodke <megha.n.bodke@gmail.com>
2022-12-17 19:35:20 +0530

    Add styles and data to views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
+ 
 root 'users#index'
-get 'users/:id', to: 'users#show', as: 'user'
-get 'users/:author_id/posts', to: 'posts#index', as: 'user_posts'
-get 'user/:author_id/posts/:id', to: 'posts#show', as: 'user_post'
+
+resources :users do 
+  resources :posts do 
+    resources :likes 
+    resources :comments 
+  end 
+end  
 
 end
